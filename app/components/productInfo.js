4193b0f5f2ce8456f2380cb2a84f52c81b9906e7
Guillermo Sequeira <gsequeira@mercadolibre.com>
2021-04-02 19:29:16 -0300

    add responsive product

--- app/components/productInfo.js
+++ app/components/productInfo.js
@@ -4,24 +4,28 @@ require('./productInfo.scss');
 const ProductsInfo = ({ title, picture, price, condition, sold_quantity, description }) => (
   <div className="products-info">
     <div className="products-info-container">
-      <div className="products-info-container-side-info">
-        <div className="products-info-container-side-info-subtitle">
-          {condition === 'new' ? 'Nuevo' : 'Usado'}
-          {sold_quantity > 0 && ` - ${sold_quantity} vendidos`}
+      <div className="products-info-container-buy">
+        <div className="products-info-container-buy-container-image">
+          <img className="products-info-container-buy-container-image-content" src={picture} alt="product-image" />
         </div>
-        <h2 className="products-info-container-side-info-title">{title}</h2>
-        <div className="products-info-container-side-info-price">
-          $ {price.amount}
-          {price.decimals > 0 && <sup>{price.decimals}</sup>}
+        <div className="products-info-container-buy-texts">
+          <div className="products-info-container-buy-texts-subtitle">
+            {condition === 'new' ? 'Nuevo' : 'Usado'}
+            {sold_quantity > 0 && ` - ${sold_quantity} vendidos`}
+          </div>
+          <h2 className="products-info-container-buy-texts-title">{title}</h2>
+          <div className="products-info-container-buy-texts-price">
+            $ {price.amount}
+            {price.decimals > 0 && <sup>{price.decimals}</sup>}
+          </div>
+          <a href="#">
+            <div className="products-info-container-buy-texts-button" >Comprar</div>
+          </a>
         </div>
-        <a href="#">
-          <div className="products-info-container-side-info-button" >Comprar</div>
-        </a>
       </div>
-      <div className="products-info-container-central-info">
-        <img className="products-info-container-central-info-image" src={picture} alt="product-image" />
-        <h3 className="products-info-container-central-info-title" >Descripción del producto</h3>
-        {description && <p className="products-info-container-central-info-description">{description}</p>}
+      <div className="products-info-container-description">
+        <h3 className="products-info-container-description-title" >Descripción del producto</h3>
+        {description && <p className="products-info-container-description-description">{description}</p>}
       </div>
     </div>
   </div>
