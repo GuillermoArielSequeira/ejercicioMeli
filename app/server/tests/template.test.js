ced614aed68129aac57b906721a08da43c6b44ab
Guillermo Sequeira <gsequeira@mercadolibre.com>
2021-04-03 19:18:50 -0300

    add tests

---
+++ app/server/tests/template.test.js
@@ -0,0 +1,22 @@
+const template = require('../template');
+
+// eslint-disable-next-line no-useless-escape
+describe('Template function', () => {
+  it('Template function works fine', () => {
+    const wrapperFuction = template('home', 'component');
+    expect(wrapperFuction).toEqual(`<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset=\"UTF-8\">
+      <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+      <meta name=\"viewport\" content=\"width=device-width\">
+      <link rel="stylesheet" type="text/css" href="/home.css">
+  </head>
+  <body>
+    <div id=\"root\">component</div>
+    <script src=\"/home.js\"></script>
+  </body>
+  </html>
+  `);
+  })
+});
