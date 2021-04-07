8e3d2c488ff18cd71770b99de93a23f682e1bba2
Guillermo Sequeira <gsequeira@mercadolibre.com>
2021-04-07 14:00:28 -0300

    add changes

--- app/server/template.js
+++ app/server/template.js
@@ -6,6 +6,8 @@ module.exports = (bundle, component) =>
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width">
       <link rel="stylesheet" type="text/css" href="/${bundle}.css">
+      <link rel="apple-touch-icon" href="../assets/favicon.ico" />
+      <title>Mercado Libre</title>
   </head>
   <body>
     <div id="root">${component}</div>
